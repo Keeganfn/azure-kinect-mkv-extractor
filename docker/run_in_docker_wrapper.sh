f52ceb74a65880d2b8e782b12b7ace17fe128e3b
Jostan86 <jostanbrown86@gmail.com>
2024-10-26 12:23:50 -0700

    Updates for docker usage

--- docker/run_in_docker_wrapper.sh
+++ docker/run_in_docker_wrapper.sh
@@ -5,8 +5,10 @@ INPUT_FILE="/path/to/input_file.mkv"
 EXTRACTION_FOLDER="/path/to/extraction_folder"
 
 # Set the run options
-# RUN_OPTIONS="-d -c -s 15 -e 17"
 RUN_OPTIONS="-a"
+# RUN_OPTIONS="-p -c -cc"
+# RUN_OPTIONS="-p -c -cc --start 3.5 --stop 5.5"
+# RUN_OPTIONS="-dcol -cr 0 1500"
 
 # Run the main script with default arguments
 ./run_in_docker.sh $INPUT_FILE $EXTRACTION_FOLDER $RUN_OPTIONS
